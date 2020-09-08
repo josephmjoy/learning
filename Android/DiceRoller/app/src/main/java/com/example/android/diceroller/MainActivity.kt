22aebc4f323fa3116b780aa8812c43521802a238
josephmjoy <josephj@rinworks.com>
2020-09-08 15:55:26 -0700

    Random dice roll (text) displayed
    When button is pressed. - Lesson 1: Part 18

--- Android/DiceRoller/app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ Android/DiceRoller/app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.android.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.roll_button);
+        rollButton.setOnClickListener{rollDice()}
+    }
+
+    private fun rollDice() {
+        val resultText: TextView = findViewById(R.id.result_text)
+        val result = (Random().nextInt(6)+ 1).toString()
+        resultText.text = result
     }
 }
