acc3651a7571d4a54d011b9bda4a3723fe94a21a
josephmjoy <josephj@rinworks.com>
2020-09-08 17:43:00 -0700

    Added lateinit field diceImage and empty start image

--- Android/DiceRoller/app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ Android/DiceRoller/app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -2,21 +2,31 @@ package com.example.android.diceroller
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var diceImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.roll_button);
         rollButton.setOnClickListener{rollDice()}
+        this.diceImage = findViewById(R.id.dice_image)
     }
 
     private fun rollDice() {
-        val resultText: TextView = findViewById(R.id.result_text)
-        val result = (Random().nextInt(6)+ 1).toString()
-        resultText.text = result
+        val res = when (Random().nextInt(6)) {
+            0 -> R.drawable.dice_1
+            1 -> R.drawable.dice_2
+            2 -> R.drawable.dice_3
+            3 -> R.drawable.dice_4
+            4 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(res)
     }
 }
